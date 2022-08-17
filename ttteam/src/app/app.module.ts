daedea3aa1c4057aa909a6cf960bec2a829a3402
Lisandro Martin <lisandro.martin@gds.ey.com>
2022-08-17 15:37:40 -0300

    componente con datos ficticios

--- ttteam/src/app/app.module.ts
+++ ttteam/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+import { NgModule } from '@angular/core';  
+import { BrowserModule } from '@angular/platform-browser';  
+import { HttpClientModule } from "@angular/common/http";  
+import { AppComponent } from './app.component';  
+import {OrganizationChartModule} from 'primeng/organizationchart';  
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  
+import { OrgchartComponent } from './org-chart/org-chart.component';  
+  
+@NgModule({  
+  declarations: [  
+    AppComponent,  
+    OrgchartComponent  
+  ],  
+  imports: [  
+    BrowserModule,HttpClientModule,OrganizationChartModule,BrowserAnimationsModule  
+  ],  
+  providers: [],  
+  bootstrap: [AppComponent]  
+})  
+export class AppModule { }  
