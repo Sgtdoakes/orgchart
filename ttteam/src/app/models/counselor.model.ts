9bd12f491556915890e4b366da7f51cedecb3344
Lisandro Martin <lisandro.martin@gds.ey.com>
2022-08-26 14:41:28 -0300

    terminado

--- ttteam/src/app/models/counselor.model.ts
+++ ttteam/src/app/models/counselor.model.ts
@@ -2,5 +2,6 @@ export class Counselor{
     name:string = "";
     email:string = "";
     rank:string = "";
+    avatar:string ="";
     counselees:Array<Counselor> = [];
 }
--- ttteam/src/app/org-chart/org-chart.component.ts
+++ ttteam/src/app/org-chart/org-chart.component.ts
@@ -1,37 +1,42 @@
 import { Component, OnInit } from '@angular/core';  
 import { TreeNode } from 'primeng/api';  
 import { Counselor } from '../models/counselor.model';
-import {OrganizationChartModule} from 'primeng/organizationchart';
 import data from './ttteam.json';
   
 @Component({  
   selector: 'app-orgchart',  
   templateUrl: './org-chart.component.html',  
-  styleUrls: ['./org-chart.component.css']  
+  styleUrls: ['./org-chart.component.css']
 })  
 
 
 export class OrgchartComponent implements OnInit {  
-  
+
   constructor() { }
-    counselorNodes:TreeNode[] = [];
-    selectedNode: TreeNode[] = [];
+    counselorNodes:TreeNode<Counselor>[] = [];
+    selectedNode: TreeNode<Counselor>[] = [];
+    counseleeCount = 0;
     ngOnInit(): void {
         this.counselorNodes.push(this.counselorToTreeNode(data)); 
+    }   
+        
+    countingCounselees(counselor: Counselor){
+        this.counseleeCount = counselor.counselees.length;
     }
+    
 
+    private counselorToTreeNode(counselor: Counselor): TreeNode {
+        let counselorTreeNodes: TreeNode[] = [];
+        
+        for (let c of counselor.counselees) {
+            counselorTreeNodes.push(this.counselorToTreeNode(c));
+        }
 
-private counselorToTreeNode(counselor: Counselor): TreeNode {
-    let counselorTreeNodes: TreeNode[] = [];
-
-    for (let c of counselor.counselees) {
-        counselorTreeNodes.push(this.counselorToTreeNode(c));
+        return {
+            label: counselor.name,
+            data: counselor,
+            children: counselorTreeNodes,
+            expanded: true
+        };    
     }
-    return {
-        label: counselor.name,
-        data: counselor,
-        children: counselorTreeNodes,
-        expanded: true
-    };
-}
 }  
